5300e3a4d638ce51641112295127aab719951652
cyanmagar <dhanalebict@gmail.com>
2024-08-06 21:48:38 +0545

    Add .gitattributes file to manage line endings

---
+++ src/main/java/com/cyan/User/entity/User.java
@@ -0,0 +1,17 @@
+package com.cyan.User.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Data
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+    private String userName;
+    private String password;
+}
