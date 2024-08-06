e942289b29ec60bd1199faa23c235a05b06f574f
cyanmagar <dhanalebict@gmail.com>
2024-08-06 21:23:10 +0545

    integrated backend

--- vite-project/src/components/Admin/AddTeacher.jsx
+++ vite-project/src/components/Admin/AddTeacher.jsx
@@ -1,42 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import teacherService from "../../Services/teacher.service";
+import Modal from "react-modal";
 
-import Modal from 'react-modal';
-
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 const AddTeacher = () => {
-  const [formData, setFormData] = useState({
-    fullName: '',
-    username: '',
-    password: '',
-    email: '',
-    contact: ''
+  const [teacher, setTeacher] = useState({
+    teacherName: "",
+    userName: "",
+    password: "",
+    teacherEmail: "",
+    teacherContact: "",
   });
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
+    setTeacher({
+      ...teacher,
+      [name]: value,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Add code to handle form submission, e.g., send form data to an API
-    console.log('Form Data:', formData);
+    teacherService
+      .saveTeacher(teacher)
+      .then((res) => {
+        setTeacher({
+          teacherName: "",
+          userName: "",
+          password: "",
+          teacherEmail: "",
+          teacherContact: "",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    console.log("Form Data:", teacher);
     setModalIsOpen(true);
   };
 
   const handleReset = () => {
-    setFormData({
-      fullName: '',
-      username: '',
-      password: '',
-      email: '',
-      contact: ''
+    setTeacher({
+      teacherName: "",
+      userName: "",
+      password: "",
+      teacherEmail: "",
+      teacherContact: "",
     });
   };
 
@@ -46,99 +60,116 @@ const AddTeacher = () => {
   };
 
   return (
-   
-        <div className="flex-1 mt-1 ml-6 mb-20 flex flex-col">
-          <div className="flex-grow bg-gray-100 border-1 border-gray-400 p-4 mt-20 ml-2">
-            <h1 className="text-2xl font-semibold mb-4">Add Teacher</h1>
-            <form onSubmit={handleSubmit} className="bg-white p-6 rounded-md shadow-md max-w-2xl mx-auto">
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="fullName">
-                  Full name
-                </label>
-                <input
-                  type="text"
-                  name="fullName"
-                  id="fullName"
-                  value={formData.fullName}
-                  onChange={handleChange}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                  Username
-                </label>
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  value={formData.username}
-                  onChange={handleChange}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="contact">
-                  Contact
-                </label>
-                <input
-                  type="text"
-                  name="contact"
-                  id="contact"
-                  value={formData.contact}
-                  onChange={handleChange}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  required
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <button
-                  type="submit"
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-                  Submit
-                </button>
-                <button
-                  type="button"
-                  onClick={handleReset}
-                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-                  Reset
-                </button>
-              </div>
-            </form>
+    <div className="flex-1 mt-1 ml-6 mb-20 flex flex-col">
+      <div className="flex-grow bg-gray-100 border-1 border-gray-400 p-4 mt-20 ml-2">
+        <h1 className="text-2xl font-semibold mb-4">Add Teacher</h1>
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          className="bg-white p-6 rounded-md shadow-md max-w-2xl mx-auto"
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="teacherName"
+            >
+              Full name
+            </label>
+            <input
+              type="text"
+              name="teacherName"
+              id="teacherName"
+              value={teacher.teacherName}
+              onChange={(e) => handleChange(e)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
           </div>
-          
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="userName"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="userName"
+              id="userName"
+              value={teacher.userName}
+              onChange={(e) => handleChange(e)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={teacher.password}
+              onChange={(e) => handleChange(e)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="teacherEmail"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="teacherEmail"
+              id="teacherEmail"
+              value={teacher.teacherEmail}
+              onChange={(e) => handleChange(e)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="teacherContact"
+            >
+              Contact
+            </label>
+            <input
+              type="text"
+              name="teacherContact"
+              id="teacherContact"
+              value={teacher.teacherContact}
+              onChange={(e) => handleChange(e)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Submit
+            </button>
+            <button
+              type="button"
+              onClick={handleReset}
+              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Reset
+            </button>
+          </div>
+        </form>
+      </div>
+
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
@@ -146,8 +177,10 @@ const AddTeacher = () => {
         className="bg-white p-6 rounded-md shadow-md mx-auto my-20 max-w-lg"
         overlayClassName="fixed inset-0 bg-black bg-opacity-50"
       >
-        <h2 className="text-xl font-semibold mb-4">Form Submitted Successfully!</h2>
-       
+        <h2 className="text-xl font-semibold mb-4">
+          Form Submitted Successfully!
+        </h2>
+
         <button
           onClick={closeModal}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
--- vite-project/src/components/Admin/ManageTeacher.jsx
+++ vite-project/src/components/Admin/ManageTeacher.jsx
@@ -1,87 +1,89 @@
-// src/components/Admin/Teacher/ManageTeacher.jsx
-import React from 'react';
-
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import teacherService from "../../Services/teacher.service";
 
 const ManageTeacher = () => {
+  const [teacherList, setTeacherList] = useState([]);
+
+  useEffect(() => {
+    fetchTeachers();
+  }, []);
+
+  const fetchTeachers = () => {
+    teacherService
+      .getAllTeacher()
+      .then((res) => {
+        console.log(res.data);
+        setTeacherList(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleDelete = (id) => {
+    teacherService
+      .deleteTeacher(id)
+      .then((res) => {
+        console.log(res.data);
+        fetchTeachers(); // Refresh the list after deletion
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-   
-        <div className="flex-1 mt-1 ml-6 mb-20 flex flex-col">
-          <div className="flex-grow bg-gray-200 border-1 border-gray-400 p-6 mt-20 ml-6">
-            <h1 className="text-2xl font-semibold mb-4">Teacher</h1>
-            <div className="flex justify-end mb-4">
-              <Link to="/admin/add-teacher">
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Add Teacher</button>
-              </Link>
-            </div>
-            <div className="table-container">
-              <h3 className="text-lg font-semibold mb-2">Current Teachers</h3>
-              <table className="min-w-full bg-white border border-gray-300">
-                <thead>
-                  <tr>
-                    <th className="py-2 px-4 border-b">#</th>
-                    <th className="py-2 px-4 border-b">Teacher Name</th>
-                    <th className="py-2 px-4 border-b">Contact</th>
-                    <th className="py-2 px-4 border-b">Email</th>
-                    <th className="py-2 px-4 border-b">Username</th>
-                    <th className="py-2 px-4 border-b">Password</th>
-                    <th className="py-2 px-4 border-b">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="py-2 px-4 border-b">1</td>
-                    <td className="py-2 px-4 border-b">Amrit Thapa</td>
-                    <td className="py-2 px-4 border-b">9008564156</td>
-                    <td className="py-2 px-4 border-b">amrit12@gmail.com</td>
-                    <td className="py-2 px-4 border-b">amrit</td>
-                    <td className="py-2 px-4 border-b">********</td>
-                    <td className="py-2 px-4 border-b">
-                      <i className="fas fa-edit mr-2"></i>
-                      <i className="fas fa-trash-alt"></i>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="py-2 px-4 border-b">2</td>
-                    <td className="py-2 px-4 border-b">Sujan Ale</td>
-                    <td className="py-2 px-4 border-b">9008564156</td>
-                    <td className="py-2 px-4 border-b">sujan123@gmail.com</td>
-                    <td className="py-2 px-4 border-b">sujan</td>
-                    <td className="py-2 px-4 border-b">********</td>
-                    <td className="py-2 px-4 border-b">
-                      <i className="fas fa-edit mr-2"></i>
-                      <i className="fas fa-trash-alt"></i>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="py-2 px-4 border-b">3</td>
-                    <td className="py-2 px-4 border-b">Anju Thapa</td>
-                    <td className="py-2 px-4 border-b">9008564156</td>
-                    <td className="py-2 px-4 border-b">aleanju1@gmail.com</td>
-                    <td className="py-2 px-4 border-b">anju</td>
-                    <td className="py-2 px-4 border-b">********</td>
-                    <td className="py-2 px-4 border-b">
-                      <i className="fas fa-edit mr-2"></i>
-                      <i className="fas fa-trash-alt"></i>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="py-2 px-4 border-b">4</td>
-                    <td className="py-2 px-4 border-b">Binita Thapa</td>
-                    <td className="py-2 px-4 border-b">9008564156</td>
-                    <td className="py-2 px-4 border-b">binitathapa@gmail.com</td>
-                    <td className="py-2 px-4 border-b">binita</td>
-                    <td className="py-2 px-4 border-b">********</td>
-                    <td className="py-2 px-4 border-b">
-                      <i className="fas fa-edit mr-2"></i>
-                      <i className="fas fa-trash-alt"></i>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          
+    <div className="flex-1 mt-1 ml-6 mb-20 flex flex-col">
+      <div className="flex-grow bg-gray-200 border-1 border-gray-400 p-6 mt-20 ml-6">
+        <h1 className="text-2xl font-semibold mb-4">Teacher</h1>
+        <div className="flex justify-end mb-4">
+          <Link to="/admin/add-teacher">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+              Add Teacher
+            </button>
+          </Link>
+        </div>
+        <div className="table-container">
+          <h3 className="text-lg font-semibold mb-2">Current Teachers</h3>
+          <table className="min-w-full bg-white border border-gray-300">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b">#</th>
+                <th className="py-2 px-4 border-b">Teacher Name</th>
+                <th className="py-2 px-4 border-b">Contact</th>
+                <th className="py-2 px-4 border-b">Email</th>
+                <th className="py-2 px-4 border-b">Username</th>
+                <th className="py-2 px-4 border-b">Password</th>
+                <th className="py-2 px-4 border-b">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {teacherList.map((e, num) => (
+                <tr key={e.id}>
+                  <td className="py-2 px-4 border-b">{num + 1}</td>
+                  <td className="py-2 px-4 border-b">{e.teacherName}</td>
+                  <td className="py-2 px-4 border-b">{e.teacherContact}</td>
+                  <td className="py-2 px-4 border-b">{e.teacherEmail}</td>
+                  <td className="py-2 px-4 border-b">{e.userName}</td>
+                  <td className="py-2 px-4 border-b">{e.password}</td>
+                  <td className="py-2 px-4 border-b">
+                    <button className="bg-green-500 text-white px-2 py-1 rounded-md hover:bg-green-600">
+                      Edit
+                    </button>
+                    <button
+                      className="bg-red-500 text-white px-2 py-1 rounded-md hover:bg-red-600 ml-2"
+                      onClick={() => handleDelete(e.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
